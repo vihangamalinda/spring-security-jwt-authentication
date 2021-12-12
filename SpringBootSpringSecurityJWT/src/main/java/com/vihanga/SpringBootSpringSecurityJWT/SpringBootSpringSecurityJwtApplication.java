d2165aa3104b253da6607eca9c2779688a2f26b5
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2021-12-12 17:15:04 +0530

    Initializing the project.

---
+++ SpringBootSpringSecurityJWT/src/main/java/com/vihanga/SpringBootSpringSecurityJWT/SpringBootSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.vihanga.SpringBootSpringSecurityJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringSecurityJwtApplication.class, args);
+	}
+
+}
